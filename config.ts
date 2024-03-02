af6a43c0909db125ce2ee8e8aa4ef39aeb0a5680
Sten Hougaard <netsi1964@gmail.com>
2024-03-02 22:37:52 +0100

    Added 4 more prompts

--- config.ts
+++ config.ts
@@ -90,4 +90,155 @@ export const prompts = [
       },
     ],
   },
+  {
+    title: "Surreal Cityscape at Night",
+    prompt:
+      "Create an image depicting a surreal cityscape at night, where the buildings twist and turn like ribbons against a starry sky. The architecture should blend futuristic elements with Art Nouveau styles, creating a scene that is both alien and familiar. The city is alive with glowing neon lights in a spectrum of colors, reflecting off twisting streets that seem to defy gravity. In the foreground, a river meanders through the city, its surface a mirror to the dazzling lights above. This scene should challenge the boundaries between reality and imagination, showcasing the AI's ability to merge architectural precision with the fluidity of dreamscapes.",
+    files: [
+      {
+        service: KnownServices.LEXICA_ART,
+        url: "../images/lexica.art/Surreal Cityscape at Night.webp",
+      },
+      {
+        service: KnownServices.CLIPDROP_CO,
+        url: "../images/clipdrop.co/Surreal Cityscape at Night.png",
+      },
+      {
+        service: KnownServices.DALL_E_3,
+        url: "../images/dall_e_3/Surreal Cityscape at Night.webp",
+      },
+      {
+        service: KnownServices.APP_LEONARDO_AI,
+        url: "../images/app.leonardo.ai/Surreal Cityscape at Night.jpg",
+      },
+      {
+        service: KnownServices.RUNWAYML_COM,
+        url: "../images/runwayml.com/Surreal Cityscape at Night.jpg",
+      },
+      {
+        service: KnownServices.PLAYGROUND_COM,
+        url: "../images/playground.com/Surreal Cityscape at Night.png",
+      },
+      {
+        service: KnownServices.MIDJOURNEY,
+        url: "../images/midjourney/Surreal Cityscape at Night.png",
+      },
+    ],
+  },
+  {
+    title: "The Awakening of an Ancient Forest",
+    prompt:
+      "Imagine an ancient forest at the moment of awakening, where every tree and plant is imbued with ethereal light. This image should capture the instant when the forest's ancient magic is rekindled, with spirits of nature visibly emerging from the flora. The trees are colossal, their roots deep and intertwined, while their leaves shimmer with a luminescent glow. A pathway leads into the heart of the forest, illuminated by floating orbs of light, inviting the viewer on a journey into the unknown. This scene should test the AI's ability to blend fantasy elements with a photorealistic natural environment, creating a sense of wonder and enchantment.",
+    files: [
+      {
+        service: KnownServices.LEXICA_ART,
+        url: "../images/lexica.art/The Awakening of an Ancient Forest.jpg",
+      },
+      {
+        service: KnownServices.CLIPDROP_CO,
+        url: "../images/clipdrop.co/The Awakening of an Ancient Forest.png",
+      },
+      {
+        service: KnownServices.DALL_E_3,
+        url: "../images/dall_e_3/The Awakening of an Ancient Forest.webp",
+        comments: "Prefixed with 'a 4 by 3 image:'",
+      },
+      {
+        service: KnownServices.APP_LEONARDO_AI,
+        url: "../images/app.leonardo.ai/The Awakening of an Ancient Forest_Leonardo_Vision_XL.jpg",
+      },
+      {
+        service: KnownServices.APP_LEONARDO_AI,
+        url: "../images/app.leonardo.ai/The Awakening of an Ancient Forest-Character-portrait.jpg",
+      },
+      {
+        service: KnownServices.RUNWAYML_COM,
+        url: "../images/runwayml.com/The Awakening of an Ancient Forest.jpg",
+      },
+      {
+        service: KnownServices.PLAYGROUND_COM,
+        url: "../images/playground.com/The Awakening of an Ancient Forest.png",
+      },
+      {
+        service: KnownServices.MIDJOURNEY,
+        url: "../images/midjourney/The Awakening of an Ancient Forest.png",
+      },
+    ],
+  },
+  {
+    title: "The Melancholy of a Rainy Parisian Street",
+    prompt:
+      "Generate an image that captures the melancholy and beauty of a rainy street in Paris in the early 20th century. The scene is set during the blue hour, with the cobblestone streets glistening under the soft glow of gas street lamps. Pedestrians with umbrellas stroll past quaint cafes and bookshops, their reflections blurred in the wet pavement. In the distance, the silhouette of the Eiffel Tower looms, partially obscured by the mist. This image should convey a sense of nostalgia and romance, challenging the AI to accurately depict historical details, weather effects, and the nuanced emotions of a bygone era.",
+    files: [
+      {
+        service: KnownServices.LEXICA_ART,
+        url: "../images/lexica.art/The Melancholy of a Rainy Parisian Street.jpg",
+      },
+      {
+        service: KnownServices.CLIPDROP_CO,
+        url: "../images/clipdrop.co/The Melancholy of a Rainy Parisian Street.png",
+      },
+      {
+        service: KnownServices.DALL_E_3,
+        url: "../images/dall_e_3/The Melancholy of a Rainy Parisian Street.webp",
+        comments: "Prefixed with 'a 4 by 3 image:'",
+      },
+      {
+        service: KnownServices.APP_LEONARDO_AI,
+        url: "../images/app.leonardo.ai/The Melancholy of a Rainy Parisian Street_Leonardo_Vision_XL.jpg",
+      },
+      {
+        service: KnownServices.APP_LEONARDO_AI,
+        url: "../images/app.leonardo.ai/The Melancholy of a Rainy Parisian Street_stable_diffusion_2.1.jpg",
+      },
+      {
+        service: KnownServices.RUNWAYML_COM,
+        url: "../images/runwayml.com/The Melancholy of a Rainy Parisian Street.jpg",
+      },
+      {
+        service: KnownServices.PLAYGROUND_COM,
+        url: "../images/playground.com/The Melancholy of a Rainy Parisian Street.png",
+      },
+      {
+        service: KnownServices.MIDJOURNEY,
+        url: "../images/midjourney/The Melancholy of a Rainy Parisian Street.png",
+      },
+    ],
+  },
+  {
+    title: "Family Vacation Bliss in Thailand",
+    prompt:
+      "Generate a photo-realistic, 16:9 cinematic photograph of a family enjoying their vacation in Thailand. The image should capture a heartwarming moment where the mother, positioned in the foreground, is taking a selfie with her family, including the father, a boy, and a girl. They should all be smiling, appearing happy and relaxed, within arm's length of the camera to create a sense of intimacy and joy. In the magnificent background, the quintessential Thai beach should unfold, showcasing powdery white sands, crystal-clear turquoise waters, and lush palm trees swaying gently in the breeze. The composition should be reminiscent of a high-quality, inspiring DSLR photo, filled with vibrant colors and the natural beauty of Thailand, encapsulating the essence of a perfect family vacation.",
+    files: [
+      {
+        service: KnownServices.LEXICA_ART,
+        url: "../images/lexica.art/Family Vacation Bliss in Thailand.jpg",
+      },
+      {
+        service: KnownServices.CLIPDROP_CO,
+        url: "../images/clipdrop.co/Family Vacation Bliss in Thailand.png",
+      },
+      {
+        service: KnownServices.DALL_E_3,
+        url: "../images/dall_e_3/Family Vacation Bliss in Thailand.webp",
+        comments: "Prefixed with 'a 4 by 3 image:'",
+      },
+      {
+        service: KnownServices.APP_LEONARDO_AI,
+        url: "../images/app.leonardo.ai/Family Vacation Bliss in Thailand_Leonardo_Vision_XL.jpg",
+      },
+      {
+        service: KnownServices.RUNWAYML_COM,
+        url: "../images/runwayml.com/Family Vacation Bliss in Thailand.jpg",
+      },
+      {
+        service: KnownServices.PLAYGROUND_COM,
+        url: "../images/playground.com/Family Vacation Bliss in Thailand.png",
+      },
+      {
+        service: KnownServices.MIDJOURNEY,
+        url: "../images/midjourney/Family Vacation Bliss in Thailand.png",
+      },
+    ],
+  },
 ];
