7fde33020a83ddb6dccef92fb9c27865800fc6a4
Sten Hougaard <netsi1964@gmail.com>
2024-04-05 22:25:52 +0200

    Added Dall-E styles

---
+++ static/dall-e-3-styles.js
@@ -0,0 +1,196 @@
+const dallE3Styles = {
+  "3D Wireframe":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2F3d-wireframe.a9010148.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "35mm film":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2F35mm-film.c2b621c7.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Abstract:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fabstract.3dda8d5b.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Acrylic:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Facrylic.96b16414.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Aerial:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Faerial.e935ce99.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Analog film":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fanalog-film.1a9aa490.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Art deco":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fart-deco.7a4014c6.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Art nouveau":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fart-nouveau.42a72185.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Artificial lighting":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fartificial-lighting.d54388f2.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Anime:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fanime.aa8ad930.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Baroque:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fbaroque.eeca6f27.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Black and white":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fblack-and-white.096590e0.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Candy:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fcandy.b6e6eef7.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Cartoon:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fcartoon.e3b358cd.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Caricature:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fcaricature.28c934da.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Cave art":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fcave-art.472e3013.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Chalk art":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fchalk-art.8c4e63be.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Charcoal:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fcharcoal.c0cf1f27.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Claymation:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fclaymation.7285905d.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Close-up":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fclose-up.8a1314e3.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Comic book":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fcomic-book.62ce0fcb.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Comic strip":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fcomic-strip.44a0e2ae.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Concept art":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fconcept-art.33046684.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Cubism:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fcubism.25bd0d62.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Crayon:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fcrayon.482118a6.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Crystal:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fcrystal.f5270bce.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Cyberpunk:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fcyberpunk.a33a0efd.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Dawn: "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fdawn.2f296588.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Digital art":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fdigital-art.e90328fa.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Dusk: "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fdusk-lighting.b65392d6.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Dutch angle":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fdutch-angle.ffd36d9d.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Dystopian:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fdystopian.cd094b8c.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Electronics:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Felectronics.c222e467.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Expressionism:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fexpressionism.10db5314.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Extreme close-up":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fextreme-close-up.577e16ae.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Fairy tale":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Ffairy-tale.e24326f0.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Fantasy:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Ffantasy.0c4ca9c5.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Fauvism:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Ffauvism.9761d1dc.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Feathers:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Ffeathers.4c5ee5df.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Flowers:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fflowers.d8d9f646.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Felt: "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Ffelt.d1fdc81b.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Film noir":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Ffilm-noir.ce99536c.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Fish-eye":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Ffish-eye.59e7768d.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Folk art":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Ffolk-art.f7812812.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Futurism:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Ffuturism.16f140ac.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Gold: "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fgold.fc79abf8.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Golden hour":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fgolden-hour.100bf29f.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Gothic:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fgothic.8dc6239c.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Graffiti:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fgraffiti.5391557c.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Hand-drawn":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fhand-drawn.8cf0fd02.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "High angle":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fhigh-angle.5c258f8f.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "High contrast":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fhigh-contrast.505f5bd2.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Ice: "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fice.f4d9d2a4.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Impressionism:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fimpressionism.5b26cc92.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Ink wash":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fink-wash.ab81ab6f.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Kawaii:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fkawaii.6b28b5bd.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Lava: "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fflowing-lava.61c548a6.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Line art":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fline-art.243de8ab.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Linocut:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Flinocut.a7c4e99b.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Lithograph:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Flithograph.0de262c6.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Low angle":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Flow-angle.f1aabcb0.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Low polygon":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Flow-polygon.e556b64d.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Minimalist:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fminimalist.c2220384.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Mirrored glass":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fmirrored-glass.e8b85344.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Moss: "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fmoss.df43f64f.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Mosaic:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fmosaic.4d9b518a.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Motion blur":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fmotion-blur.924cd3cc.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Muted:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fmuted.96715b42.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Neon lights":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fneon-lights.6ebe0106.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Night:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fnight.b372773e.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Oil painting":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Foil-painting.13243692.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Origami:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Forigami.09da3d00.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Pastel:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fpastel.16d4310a.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Pencil sketch":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fpencil-sketch.1ed59059.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Pinhole camera":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fpinhole-camera.a62d73c5.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Pixel art":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fpixel-art.f4bee81b.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Pointillism:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fpointillism.4d58b842.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Stone:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fpolished-stone.686971a9.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Pop art":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fpop-art.78079130.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Retro:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fretro.bfd67ab0.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Rococo:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Frococo.2c6f22c5.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Sci-fi":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fscifi.f585fdc2.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Sculpture:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fsculpture.fde46ee8.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Sepia:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fsepia.adf47c2a.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Silk screen":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fsilk-screen.32efc1fc.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Solarpunk:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fsolarpunk.fa319cb3.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Stars:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fstars.a13cec86.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Steampunk:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fsteampunk.baf1a98a.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Surrealism:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fsurrealism.96ef0c53.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Synthwave:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fsynthwave.9c356507.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Tapestry:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Ftapestry.a5070277.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Ukiyo-e":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fukiyo-e.585a6caa.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Victorian:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fvictorian.bdcb705a.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Watercolor:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fwatercolor.ae5d19b8.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  "Wide-angle":
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fwide-angle.982cbf3f.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+  Woodcut:
+    "https://chat.openai.com//_next/image?url=https%3A%2F%2Fcdn.oaistatic.com%2F_next%2Fstatic%2Fmedia%2Fwoodcut.e3a75dc4.jpg&w=128&q=75&dpl=0ba5f26bc98b23c749ead07bb78a1cdd28d3786e",
+};
+/**
+ Run this script on Dall-e page:
+    var styles = {};
+    $$('[srcset^="/_next/image"]').forEach(ele => {
+        let {alt, srcset} = ele;
+        styles[alt] = `https://chat.openai.com/${srcset.split(' 1x, ')[0]}`
+    })
+    copy(styles)
+*/
